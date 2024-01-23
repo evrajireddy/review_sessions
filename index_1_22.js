c0dc4f1da02ae0f0f38e098f9392f1a9cec1440d
Venkata Raji Reddy Eda <evrajireddy@gmail.com>
2024-01-23 18:51:34 -0500

    first commit in index_1_22.js

--- index_1_22.js
+++ index_1_22.js
@@ -1,6 +1,17 @@
 // 1.----------------------------------
 // Write a function that takes in a string and returns a new string with every word capitalized. Write your own examples to call the function with.
 
+function formatGreeting(string){
+
+  let newString = "";
+
+  for(let i = 0; i<string.length; i++){
+
+    newString=string.upper()
+
+  }
+  return newString;
+}
 // 2.----------------------------------
 // Write a function that takes in an object and returns a formatted greeting.
 // This is the format of the object - 
@@ -18,10 +29,8 @@ const rebecca = {
   occupation: "Teacher"
 }
 
-console.log(formatGreeting(john) === "Hello John Klaus, have a great day at your job as a Carpenter")
-
-console.log(formatGreeting(rebecca) === "Hello Rebecca Cohen, have a great day at your job as a Teacher")
-
+console.log(formatGreeting(john) === "Hello John Klaus, have a great day at your job as a Carpenter")nsole.log(formatGreeting(rebecca) === "Hello Rebecca Cohen, have a great day at your job as a Teacher")
+/*
 // 3.----------------------------------
 // Write a function that takes an array of objects and a targetId and returns a formatted string. Each object in the array will have the same structure as the objects from the previous question plus an id. Use chatGPT to create an array of 10 such objects.
 
