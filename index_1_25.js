ed9f07cde51fd85d338d4a6420a30476ed274a6e
Venkata Raji Reddy Eda <evrajireddy@gmail.com>
2024-01-27 15:46:24 -0500

    updating the marketing department head name

--- index_1_25.js
+++ index_1_25.js
@@ -131,8 +131,25 @@ const companyOrganizationalStructure = {
   };
   
   // YOUR CODE HERE
-  
 
+  
+//   a. Log the desk of the second software engineer in the development department
+//   b. Log the location of the head of the marketing department
+//   c. Log the name of the CEO's executive assistant
+//   d. Add yourself as a third software engineer to the development office
+//   e. Update the name of the head of marketing to "Johann Marketer"
+
+   console.log(employee = companyOrganizationalStructure.departments.development.head.office.employees.softwareEngineer2.desk);
+  
+  console.log(location = companyOrganizationalStructure.departments.marketing.head.office.location);
+console.log(executiveAssistant = companyOrganizationalStructure.CEO.office.employees.executiveAssistant.name);
+
+ companyOrganizationalStructure.departments.development.head.office.employees.softwareEngineer3 = {
+  name: "venkata",
+  desk: "B803",
+};
+console.table(companyOrganizationalStructure.departments.development.head.office.employees);
+console.log( companyOrganizationalStructure.departments.marketing.head.name = "Johann Marketer");
 // 5. Given the following variables, create a new object. the key variable should be the key and the value variable should be it's corresponding value. You have to use the variables!
 
  key = "name";
