4312a256bae328c38df94cf942afe77adad9ab82
Venkata Raji Reddy Eda <evrajireddy@gmail.com>
2024-01-28 09:44:49 -0500

    native array method

---
+++ index_1_26.js
@@ -0,0 +1,38 @@
+// Iterative Native Array Methods
+
+// For each of the following exercises, 
+// 1. write them using a for loop first
+// 2. comment out the accumulator pattern solution and write it using a native array method
+// 3. call the function and console.log the result
+
+// 1. Write a function that takes in an array of strings and returns a new array where each string is replaced by it's length
+// ["hello", "world"] => [5, 5] 
+
+function replacingArray(arrayString){
+
+    let newarrayString = [];
+     let newString ='newString'.length = 5;
+
+    for(let i=0; i<arrayString.length; i++){
+
+        newarrayString.push(arraystring_replace("arrayString[i]", "newString"))
+
+
+
+    }
+
+    return newarrayString;
+
+}
+
+console.log(replacingArray('arrayString'));
+
+// 2. Write a function that takes an array of strings and returns an array of strings longer than 5 characters (spaces count)
+
+// 3. Write a function that takes an array of strings, and returns a new array of the lengths of strings longer than 5 characters
+
+// 4. Write a function that takes an array of numbers and returns the sum of all even numebrs
+
+// 5. Write a function that takes an array of numbers and returns the min num
+
+// 6. Write a function that takes in a string and returns the character that appears the most times (only letters count)
